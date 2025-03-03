de3183c2e331523d931185677728e7b89feed587
Rhaven Kurosaia <105242602+Rhaven3@users.noreply.github.com>
2025-03-03 13:01:37 +0000

    command ping latency unverified

--- config/config.js
+++ config/config.js
@@ -1,4 +1,17 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
 module.exports = {
+	// Instance Client
+	client: new Client({
+		intents: [
+			GatewayIntentBits.Guilds,
+			GatewayIntentBits.GuildMessages,
+			GatewayIntentBits.MessageContent,
+		],
+		allowedMentions: { parse: [] },
+		presence: { status: 'online' },
+	}),
+
 	// Initiative
 	playerDataRange: 'Etat!A1:P17',
 	buttonTime: 3_600_000,
