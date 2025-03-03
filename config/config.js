55689fe95eabf9dd8d49381324df485f3b77a7e8
Rhaven Kurosaia <105242602+Rhaven3@users.noreply.github.com>
2025-03-03 14:27:26 +0000

    update deploy-command.js & config.js

--- config/config.js
+++ config/config.js
@@ -14,8 +14,8 @@ module.exports = {
 
 	// Initiative
 	playerDataRange: 'Etat!A1:P17',
-	buttonTime: 3_600_000,
-	modalTime: 3_600_000,
+	buttonTimeMilliSecond: 3_600_000,
+	modalTimeMilliSecond: 3_600_000,
 	idSheetSpliter: ', ',
 	numberTurnByPass: 1,
 	defaultInitiative: -999,
